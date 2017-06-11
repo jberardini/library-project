f1453f0177f3652b094050e02e677f274a433937
Ryan Barry <ryan@nuclearice.com>
2017-06-10 21:57:07 -0700

    use GoodreadsApi search method

--- main.py
+++ main.py
@@ -24,11 +24,7 @@ def get_goodreads_book_id(books_list):
 	book_ids = []
 
 	for book_title, author in books_list.iteritems():
-		payload = {'key': GOODREADS_KEY, 'q': book_title}
-		response = requests.get("https://www.goodreads.com/search/index.xml", params=payload)
-		search_result = xmltodict.parse(response.text)
-		all_matches = search_result['GoodreadsResponse']['search']['results']['work']
-		
+		all_matches = gr.search_by_title(book_title)
 
 		for match in all_matches:
 			goodreads_author_name = match['best_book']['author']['name']
